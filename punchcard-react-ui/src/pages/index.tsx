12a406c31d537b4a8fb5df0097c2e3f48813764d
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-15 00:15:52 -0600

    fix margin zero issue

--- punchcard-react-ui/src/pages/index.tsx
+++ punchcard-react-ui/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "../router";
+import styles from "./Home.module.css"
 
 export default function IndexPage() {
-  return <Navigate to="/login">Redirecting...</Navigate>;
+  return <Navigate className={styles.this_is_only_to_get_the_zero_margin_line_to_run} to="/login">Redirecting...</Navigate>;
 }
