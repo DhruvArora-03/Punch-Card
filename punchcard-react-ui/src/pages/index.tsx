8d335b0bea1be1b05c1397a6f24076fc3b943f8c
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-15 00:16:59 -0600

    wrong filename

--- punchcard-react-ui/src/pages/index.tsx
+++ punchcard-react-ui/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "../router";
-import styles from "./Home.module.css"
+import styles from "./index.module.css"
 
 export default function IndexPage() {
   return <Navigate className={styles.this_is_only_to_get_the_zero_margin_line_to_run} to="/login">Redirecting...</Navigate>;
