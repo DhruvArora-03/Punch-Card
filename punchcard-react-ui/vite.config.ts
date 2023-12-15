712b12bcdbef237eb8376afca85c70089c623f00
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-14 20:14:01 -0600

    finalllyyyy fix importing issue

--- punchcard-react-ui/vite.config.ts
+++ punchcard-react-ui/vite.config.ts
@@ -4,5 +4,10 @@ import generouted from '@generouted/react-router/plugin'
 
 export default defineConfig({
   plugins: [react(), generouted()],
-  
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@lib': '/src/lib'
+    }
+  }
 });
