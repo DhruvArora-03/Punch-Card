9c6f6bb65e0ce8a0d5679085c2867452fedc2408
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-18 16:46:19 -0600

    add nav bar modes

--- punchcard-react-ui/src/components/NavBar/NavBar.tsx
+++ punchcard-react-ui/src/components/NavBar/NavBar.tsx
@@ -1,13 +1,20 @@
 import { Link } from 'router'
 import styles from './NavBar.module.css'
 import logo from 'assets/gideon-logo.png'
+import { NavBarModes } from '.';
 
-export default function NavBar() {
+
+type NavBarProps = {
+  mode?: NavBarModes;
+}
+
+export default function NavBar({ mode }: NavBarProps) {
   return <div className={styles.navbar}>
     <img src={logo} className={styles.logo} />
     <div className={styles.navbarItems}>
       <Link to="/login">Past Payments</Link>
       <Link to="/login">Settings</Link>
+      {mode !== NavBarModes.UnAuthenticated && <Link to="/logout">Log Out</Link>}
     </div>
   </div>
 }
--- punchcard-react-ui/src/components/NavBar/index.ts
+++ punchcard-react-ui/src/components/NavBar/index.ts
@@ -1,3 +1,5 @@
 import NavBar from "./NavBar";
 
+export enum NavBarModes { Default = "default", UnAuthenticated = "unauthed" };
+
 export default NavBar;
