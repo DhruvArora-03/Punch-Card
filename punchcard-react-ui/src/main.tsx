3516310994dc01609cc0f7b46796a005f62bd81d
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-18 22:41:36 -0600

    using formik and fixed version of react-auth-kit

--- punchcard-react-ui/src/main.tsx
+++ punchcard-react-ui/src/main.tsx
@@ -1,14 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import { Routes } from '@generouted/react-router'
 import React from 'react'
-import AuthProvider from 'react-auth-kit/AuthProvider';
-import createStore from 'react-auth-kit/createStore';
-
-const store = createStore<object>({ authType: "cookie", authName: "_auth", cookieDomain: window.location.hostname, cookieSecure: false })
+import { AuthProvider } from 'react-auth-kit';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider store={store}>
+    <AuthProvider
+      authType='cookie'
+      authName='_auth'
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
       <Routes />
     </AuthProvider>
   </React.StrictMode>)
--- punchcard-react-ui/src/pages/(unauthed)/login.tsx
+++ punchcard-react-ui/src/pages/(unauthed)/login.tsx
@@ -1,23 +1,68 @@
 import styles from "./login.module.css"
-import RBForm from 'react-bootstrap/Form';
+import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button'
-// import { Formik, Field, Form } from "RBFormik";
+import { useFormik } from "Formik";
+import { useSignIn } from 'react-auth-kit'
+import { useState } from "react";
+import axios, { AxiosError } from "axios";
 
 export default function LoginPage() {
+  const [error, setError] = useState("");
+  const signIn = useSignIn();
+
+  const onSubmit = async (values: any) => {
+    console.log("Values: ", values);
+    setError("");
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/login",
+        values
+      );
+
+      signIn({
+        token: response.data.token,
+        tokenType: "Bearer",
+        expiresIn: 300,
+        authState: { username: values.username },
+      });
+    } catch (err) {
+      if (err && err instanceof AxiosError)
+        setError(err.response?.data.message);
+      else if (err && err instanceof Error) setError(err.message);
+
+      console.log("Error: ", err);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: ""
+    },
+    onSubmit
+  })
+
   return <div className={styles.page}>
-    <RBForm className={styles.mainArea}>
+    <Form className={styles.mainArea} onSubmit={formik.handleSubmit}>
       <h1 className={styles.title}>Log In</h1>
       <InputGroup className={styles.input}>
         <InputGroup.Text> Username: </InputGroup.Text>
-        <RBForm.Control />
+        <Form.Control
+          onChange={formik.handleChange}
+          value={formik.values.username}
+        />
       </InputGroup>
       <InputGroup className={styles.input}>
         <InputGroup.Text> Password: </InputGroup.Text>
-        <RBForm.Control />
+        <Form.Control
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
       </InputGroup>
       <Button className={styles.button} /* onClick={() =>  signIn({ username: "temp" }) }*/> Submit </Button>
-    </RBForm>
+    </Form>
   </div>
 
 }
