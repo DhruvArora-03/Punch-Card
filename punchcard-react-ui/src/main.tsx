77605860a1f45d74a5df80621c54691c8a840b4a
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-18 22:20:23 -0600

    clean up AuthProvider

--- punchcard-react-ui/src/main.tsx
+++ punchcard-react-ui/src/main.tsx
@@ -1,20 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { Routes } from '@generouted/react-router'
 import React from 'react'
-import AuthProvider from 'react-auth-kit';
+import AuthProvider from 'react-auth-kit/AuthProvider';
 import createStore from 'react-auth-kit/createStore';
 
 const store = createStore<object>({ authType: "cookie", authName: "_auth", cookieDomain: window.location.hostname, cookieSecure: false })
 
-// authType = "cookie"
-// authName = "_auth"
-// cookieDomain = {  }
-// cookieSecure
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider
-      store={store}
-    >
+    <AuthProvider store={store}>
       <Routes />
     </AuthProvider>
   </React.StrictMode>)
