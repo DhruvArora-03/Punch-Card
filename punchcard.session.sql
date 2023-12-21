5a48bd3ac36aa7495dd54b30c380ba004cd010db
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-20 19:25:57 -0600

    change sql table design

--- punchcard.session.sql
+++ punchcard.session.sql
@@ -1,45 +1,51 @@
--- @block drop all tables
-DROP TABLE users;
-DROP TABLE shifts;
-DROP TABLE paychecks;
-
-
-
--- @block Create User Table
+-- @block create tables
 CREATE TABLE `users`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(55) NOT NULL UNIQUE,
-    `password` VARCHAR(55) NOT NULL,
-    `first_name` VARCHAR(55) NOT NULL,
-    `last_name` VARCHAR(55) NOT NULL,
-    `hourly_pay` DECIMAL(6, 4) NOT NULL,
-    `is_admin` TINYINT(1) NOT NULL,
-    `payment_method` VARCHAR(255) NOT NULL
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    `first_name` VARCHAR(255) NOT NULL,
+    `last_name` VARCHAR(255) NOT NULL,
+    `hourly_pay` DECIMAL(8, 2) NOT NULL,
+    `role` VARCHAR(255) NOT NULL,
+    `preferred_payment_method` VARCHAR(255) NULL,
+    `createdBy` BIGINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedBy` BIGINT UNSIGNED NOT NULL,
+    `updatedAt` DATETIME NOT NULL
 );
-
-
--- @block Create Paycheck Table
 CREATE TABLE `paychecks`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT UNSIGNED NOT NULL,
-    `start_date` DATE NOT NULL,
-    `end_date` DATE NOT NULL,
-    `hours_worked` DECIMAL(7, 4) NOT NULL,
-    `payment_amount` DECIMAL(9, 4) NOT NULL,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `hours_worked` DECIMAL(8, 2) NOT NULL,
+    `payment_amount` DECIMAL(8, 2) NOT NULL,
     `payment_date` DATE NOT NULL,
     `payment_method` VARCHAR(255) NOT NULL,
+    `createdBy` BIGINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedBy` BIGINT UNSIGNED NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
     FOREIGN KEY(`user_id`) REFERENCES `users`(`id`)
 );
-
--- @block Create Shift Table
 CREATE TABLE `shifts`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT UNSIGNED NOT NULL,
-    `paycheck_id` INT UNSIGNED NOT NULL,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `paycheck_id` BIGINT UNSIGNED NULL,
     `clock_in` DATETIME NOT NULL,
     `clock_out` DATETIME NULL,
-    `employee_notes` TEXT NULL,
+    `user_notes` TEXT NULL,
     `admin_notes` TEXT NULL,
+    `createdBy` BIGINT UNSIGNED NOT NULL,
+    `createdAt` DATETIME NOT NULL,
+    `updatedBy` BIGINT UNSIGNED NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
     FOREIGN KEY(`paycheck_id`) REFERENCES `paychecks`(`id`),
     FOREIGN KEY(`user_id`) REFERENCES `users`(`id`)
 );
+
+-- @block index user_ids
+CREATE INDEX idx_user_id ON shifts (user_id);
+CREATE INDEX idx_user_id ON paychecks (user_id);
+
+
+-- -- @block
+-- CREATE FUNCTION 
