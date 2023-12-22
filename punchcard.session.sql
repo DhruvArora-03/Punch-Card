ef13ef2ef939002ccb5c56315c654fe3a05862b4
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-21 18:01:11 -0600

    create procedures

--- punchcard.session.sql
+++ punchcard.session.sql
@@ -1,13 +1,17 @@
 -- @block create tables
+DROP TABLE IF EXISTS shifts;
+DROP TABLE IF EXISTS paychecks;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(55) NOT NULL UNIQUE,
-    password VARCHAR(55) NOT NULL,
-    salt VARCHAR(55) NOT NULL,
-    first_name VARCHAR(55) NOT NULL,
-    last_name VARCHAR(55) NOT NULL,
+    username VARCHAR(63) NOT NULL UNIQUE,
+    hashed_password VARCHAR(63) NOT NULL,
+    salt VARCHAR(63) NOT NULL,
+    first_name VARCHAR(63) NOT NULL,
+    last_name VARCHAR(63) NOT NULL,
     hourly_pay DECIMAL(8, 2) NOT NULL,
-    role VARCHAR(55) NOT NULL,
+    role VARCHAR(63) NOT NULL,
     preferred_payment_method VARCHAR(255) NULL,
     createdBy BIGINT UNSIGNED NOT NULL,
     createdAt DATETIME NOT NULL,
@@ -48,5 +52,66 @@ CREATE TABLE shifts (
 CREATE INDEX idx_user_id ON shifts (user_id);
 CREATE INDEX idx_user_id ON paychecks (user_id);
 
--- -- @block
--- CREATE FUNCTION 
+-- @block create procedures
+
+-- Input: username
+-- Output: corresponding id, hashed_password, and salt
+DROP PROCEDURE IF EXISTS GetUserCredentials;
+CREATE PROCEDURE GetUserCredentials(
+    IN in_username VARCHAR(55),
+    OUT user_id_result BIGINT UNSIGNED,
+    OUT hashed_password_result VARCHAR(55),
+    OUT user_salt_result VARCHAR(55)
+)
+BEGIN
+    -- Retrieve user credentials based on the provided username
+    SELECT id, hashed_password, salt
+    INTO user_id_result, hashed_password_result, user_salt_result
+    FROM users
+    WHERE username = in_username;
+END;
+
+-- Input: username, hashed_password, salt, first_name, last_name, hourly_pay, role, preferred_payment_method, creator_id
+-- Output: None
+DROP PROCEDURE IF EXISTS CreateUser;
+CREATE PROCEDURE CreateUser(
+    IN in_username VARCHAR(63),
+    IN in_hashed_password VARCHAR(63),
+    IN in_salt VARCHAR(63),
+    IN in_first_name VARCHAR(63),
+    IN in_last_name VARCHAR(63),
+    IN in_hourly_pay DECIMAL(8, 2),
+    IN in_role VARCHAR(63),
+    IN in_preferred_payment_method VARCHAR(255),
+    IN in_creator_id BIGINT UNSIGNED
+)
+BEGIN
+    -- Insert the new user into the 'users' table
+    INSERT INTO users (
+        username,
+        hashed_password,
+        salt,
+        first_name,
+        last_name,
+        hourly_pay,
+        role,
+        preferred_payment_method,
+        createdBy,
+        createdAt,
+        updatedBy,
+        updatedAt
+    ) VALUES (
+        in_username,
+        in_hashed_password,
+        in_salt,
+        in_first_name,
+        in_last_name,
+        in_hourly_pay,
+        in_role,
+        in_preferred_payment_method,
+        in_creator_id,
+        NOW(),
+        in_creator_id,
+        NOW()
+    );
+END
