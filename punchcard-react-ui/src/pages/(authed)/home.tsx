79077bd9796ca2cdf9e18225ef5cc95988fb0a50
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-23 16:10:13 -0600

    add loading state

--- punchcard-react-ui/src/pages/(authed)/home.tsx
+++ punchcard-react-ui/src/pages/(authed)/home.tsx
@@ -1,3 +1,34 @@
+import axios from "axios";
+import { useMemo, useState } from "react";
+import { Button, Spinner } from "react-bootstrap";
+import { useAuthHeader } from "react-auth-kit"
+
 export default function HomePage() {
-  return <h1>this is home page to be shown after user is logged in</h1>
+  const authHeader = useAuthHeader();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useMemo(() => {
+    setIsLoading(true)
+    axios.get("http://localhost:8080/protected", {
+      headers: {
+        Authorization: authHeader()
+      }
+    }).then(() =>
+      setIsLoading(false)
+    ).catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return <>
+    <h1>
+      this is home page to be shown after user is logged in
+    </h1>
+    <Button>Clock In</Button>
+  </>
+
 }
