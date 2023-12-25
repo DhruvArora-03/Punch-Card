4bccac11546282184d416fa08afc3de6e9659e34
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-25 03:59:49 -0600

    refactor

--- api/db/db.go
+++ api/db/db.go
@@ -5,7 +5,26 @@ import (
 )
 
 var db *sql.DB
-var getUserCredentials *sql.Stmt
+
+func ConnectToDB() (*sql.DB, error) {
+	// Connection parameters
+	dsn := "admin:admin@tcp(localhost:3306)/punchcard"
+
+	// Open a connection to the MySQL database
+	var err error;
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
 
 func GetUserCredentials(username string) (int64, string, string, error) {
 	rows, err := db.Query("CALL GetUserCredentials(?, @user_id, @user_hashed_password, @user_salt)", username)
@@ -34,23 +53,6 @@ func GetUserCredentials(username string) (int64, string, string, error) {
 	return id.Int64, hashPass.String, salt.String, nil
 }
 
-func ConnectToDB() (*sql.DB, error) {
-	// Connection parameters
-	dsn := "admin:admin@tcp(localhost:3306)/punchcard"
-
-	// Open a connection to the MySQL database
-	var err error;
-	db, err = sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	// Test the connection
-	err = db.Ping()
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+func ClockIn(userID string) {
+	
 }
-
