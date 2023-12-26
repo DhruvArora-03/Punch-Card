485580d6ca40b20910e4be30d434220678655c0b
DhruvArora-03 <93348668+DhruvArora-03@users.noreply.github.com>
2023-12-26 02:45:46 -0600

    turn image into a link

--- punchcard-react-ui/src/components/NavBar/NavBar.tsx
+++ punchcard-react-ui/src/components/NavBar/NavBar.tsx
@@ -13,13 +13,16 @@ export default function NavBar({ mode }: NavBarProps) {
   const signOut = useSignOut();
 
   return <div className={styles.navbar}>
-    <img src={logo} className={styles.logo} />
+    <Link to="/home">
+      <img src={logo} className={styles.logo} />
+    </Link>
+
     <div className={styles.navbarItems}>
       {mode !== NavBarModes.UnAuthenticated &&
         <>
-          <Link to="/">History</Link>
-          <Link to="/">Payments</Link>
-          <Link to="/">Settings</Link>
+          <Link to="/history">History</Link>
+          <Link to="/payments">Payments</Link>
+          <Link to="/settings">Settings</Link>
           <Link to="/login" onClick={() => { signOut() }}>Log Out</Link>
         </>
       }
