2e586b2d4bd8f53c4bde72aa297f05b2627c6dfd
DhruvArora-03 <dhruvarora@utexas.edu>
2024-01-09 15:23:05 -0600

    autoformat

--- punchcard-react-ui/src/pages/(unauthed)/login.tsx
+++ punchcard-react-ui/src/pages/(unauthed)/login.tsx
@@ -1,10 +1,10 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import { useFormik } from 'Formik';
-import { useState } from 'react';
-import { useSignIn } from 'react-auth-kit';
-import { Form, InputGroup } from 'react-bootstrap';
-import styles from './login.module.css';
-import Button from 'components/Button';
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useFormik } from "Formik";
+import { useState } from "react";
+import { useSignIn } from "react-auth-kit";
+import { Form, InputGroup } from "react-bootstrap";
+import styles from "./login.module.css";
+import Button from "components/Button";
 
 export default function LoginPage() {
   const [error, setError] = useState<Error | null>(null);
@@ -16,68 +16,82 @@ export default function LoginPage() {
     // encode
     const passwordBuffer = encoder.encode(password);
     // hash
-    return window.crypto.subtle.digest("SHA-256", passwordBuffer)
-      // decode
-      .then((hashBuffer) => Array.from(new Uint8Array(hashBuffer)).map(byte => byte.toString(16).padStart(2, '0')).join(''))
+    return (
+      window.crypto.subtle
+        .digest("SHA-256", passwordBuffer)
+        // decode
+        .then((hashBuffer) =>
+          Array.from(new Uint8Array(hashBuffer))
+            .map((byte) => byte.toString(16).padStart(2, "0"))
+            .join("")
+        )
+    );
   };
 
-  const onSubmit = async (values: any) => {
-    setError(null)
-    setIsLoading(true)
+  const onSubmit = async (values: { username: string; password: string }) => {
+    setError(null);
+    setIsLoading(true);
 
     await hashPassword(values.password)
-      .then((hashedPassword: string) => axios.post(
-        "http://localhost:8080/login",
-        {
+      .then((hashedPassword: string) =>
+        axios.post("http://localhost:8080/login", {
           username: values.username,
-          password: hashedPassword
-        }
-      ))
-      .then((response: AxiosResponse) => signIn({
-        token: response.data,
-        tokenType: "Bearer",
-        expiresIn: 300,
-        authState: { username: values.username },
-      }))
+          password: hashedPassword,
+        })
+      )
+      .then((response: AxiosResponse) =>
+        signIn({
+          token: response.data,
+          tokenType: "Bearer",
+          expiresIn: 300,
+          authState: { username: values.username },
+        })
+      )
       .catch((err) => {
-        setError(err)
+        setError(err);
         console.log("Error Message: ", err.message);
       })
-      .then(() => setIsLoading(false))
+      .then(() => setIsLoading(false));
   };
 
   const formik = useFormik({
     initialValues: {
       username: "",
-      password: ""
+      password: "",
     },
-    onSubmit
-  })
+    onSubmit,
+  });
 
-  return <div className={styles.page}>
-    <Form className={styles.mainArea} onSubmit={formik.handleSubmit}>
-      <h1 className={styles.title}>Log In</h1>
-      <InputGroup className={styles.input}>
-        <InputGroup.Text> Username: </InputGroup.Text>
-        <Form.Control
-          id="username"
-          autoComplete="on"
-          onChange={formik.handleChange}
-          value={formik.values.username}
+  return (
+    <div className={styles.page}>
+      <Form className={styles.mainArea} onSubmit={formik.handleSubmit}>
+        <h1 className={styles.title}>Log In</h1>
+        <InputGroup className={styles.input}>
+          <InputGroup.Text> Username: </InputGroup.Text>
+          <Form.Control
+            id="username"
+            autoComplete="on"
+            onChange={formik.handleChange}
+            value={formik.values.username}
+          />
+        </InputGroup>
+        <InputGroup className={styles.input}>
+          <InputGroup.Text> Password: </InputGroup.Text>
+          <Form.Control
+            id="password"
+            type="password"
+            autoComplete="on"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+        </InputGroup>
+        <Button
+          className={styles.button}
+          type="submit"
+          loading={isLoading}
+          text="Submit"
         />
-      </InputGroup>
-      <InputGroup className={styles.input}>
-        <InputGroup.Text> Password: </InputGroup.Text>
-        <Form.Control
-          id="password"
-          type="password"
-          autoComplete="on"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-        />
-      </InputGroup>
-      <Button className={styles.button} type="submit" loading={isLoading} text="Submit" />
-    </Form>
-  </div>
-
+      </Form>
+    </div>
+  );
 }
