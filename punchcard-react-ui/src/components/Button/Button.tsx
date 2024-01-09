0602c08f732f52aeb85678cfa3119a5aa93cc0dd
DhruvArora-03 <dhruvarora@utexas.edu>
2024-01-09 15:50:32 -0600

    add outline option

--- punchcard-react-ui/src/components/Button/Button.tsx
+++ punchcard-react-ui/src/components/Button/Button.tsx
@@ -3,35 +3,34 @@ import styles from "./Button.module.css";
 import { Button, Spinner } from "react-bootstrap";
 
 type ButtonProps = {
-  className?: string,
-  text?: string,
-  disabled?: boolean,
-  loading?: boolean,
-  type?: "button" | "submit" | "reset",
-  color?: "blue" | "red" | "green" | "yellow" | "gray",
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  className?: string;
+  text?: string;
+  disabled?: boolean;
+  loading?: boolean;
+  type?: "button" | "submit" | "reset";
+  color?: "blue" | "red" | "green" | "yellow" | "gray";
+  outline?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 const convert = {
-  "blue": "primary",
-  "red": "danger",
-  "green": "success",
-  "yellow": "warning",
-  "gray": "secondary"
-}
+  blue: "primary",
+  red: "danger",
+  green: "success",
+  yellow: "warning",
+  gray: "secondary",
+};
 
 export default function ButtonComponent(props: ButtonProps) {
-  return <Button
-    className={props.className}
-    variant={convert[props.color ?? "blue"]}
-    disabled={props.disabled || props.loading}
-    type={props.type}
-    onClick={props.onClick}
-  >
-    {props.loading
-      ? <Spinner
-        size="sm"
-      />
-      : props.text}
-  </Button>
+  return (
+    <Button
+      className={props.className}
+      variant={(props.outline ? "outline-" : "") + convert[props.color ?? "blue"]}
+      disabled={props.disabled || props.loading}
+      type={props.type}
+      onClick={props.onClick}
+    >
+      {props.loading ? <Spinner size="sm" /> : props.text}
+    </Button>
+  );
 }
