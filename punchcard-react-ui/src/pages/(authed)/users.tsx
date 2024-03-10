8332ccb7a7cba3d151e4b84143648b47520915c8
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-09 23:16:48 -0600

    add title/background

--- punchcard-react-ui/src/pages/(authed)/users.tsx
+++ punchcard-react-ui/src/pages/(authed)/users.tsx
@@ -1,7 +1,9 @@
-import styles from './home.module.css';
+import styles from "./users.module.css";
 
 export default function ManageUsersPage() {
-  return <>
-    <h1>blah</h1>
-  </>
+  return (
+    <div className={styles.page}>
+      <h1>Manage Users</h1>
+    </div>
+  );
 }
