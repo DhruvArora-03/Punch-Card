33ae45dfaa0375601e25d91c8250a6e95c1bf3b6
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-09 23:18:28 -0600

    add semicolon

--- punchcard-react-ui/src/pages/(authed)/history.tsx
+++ punchcard-react-ui/src/pages/(authed)/history.tsx
@@ -62,7 +62,7 @@ export default function HistoryPage() {
               })
             );
           } else {
-            setData([])
+            setData([]);
           }
         });
       setIsLoading(false);
