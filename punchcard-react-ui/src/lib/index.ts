2763cb4b562630fe6af2c2180b87235e136cdc50
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-10 20:46:35 -0500

    run lint and rewrite apiWrapper

--- punchcard-react-ui/src/lib/index.ts
+++ punchcard-react-ui/src/lib/index.ts
@@ -1,14 +1,14 @@
 import { format, isToday, isYesterday } from "date-fns";
 
-export type setStateType<T> = React.Dispatch<React.SetStateAction<T>>
+export type setStateType<T> = React.Dispatch<React.SetStateAction<T>>;
 export type historyRowType = {
-  key: string
+  key: string;
   clock_in_time: string;
   clock_out_time: string;
   duration: number;
   user_notes: string;
   admin_notes: string;
-}
+};
 export type userDataType = {
   user_id: number;
   username: string;
@@ -17,18 +17,34 @@ export type userDataType = {
   hourly_pay: number;
   role: string;
   preferred_payment_method: string;
+};
+
+export async function apiWrapper(
+  apiCall: () => Promise<void>,
+  setError: setStateType<Error | null>,
+  setIsLoading: setStateType<boolean>
+) {
+  setIsLoading(true);
+  await apiCall()
+    .catch((err) => setError(err))
+    .then(() => setIsLoading(false));
 }
 
 export function formatClockedInMessage(date: Date) {
   const currentDate = new Date();
 
   if (isToday(date)) {
-    return `You clocked in at ${format(date, 'h:mm a')} today.`;
+    return `You clocked in at ${format(date, "h:mm a")} today.`;
   } else if (isYesterday(date)) {
-    return `You clocked in at ${format(date, 'h:mm a')} yesterday.`;
+    return `You clocked in at ${format(date, "h:mm a")} yesterday.`;
   } else {
-    const daysDifference = Math.floor((currentDate.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
-    return `You clocked in at ${format(date, 'h:mm a')} ${daysDifference} days ago.`;
+    const daysDifference = Math.floor(
+      (currentDate.getTime() - date.getTime()) / (1000 * 60 * 60 * 24)
+    );
+    return `You clocked in at ${format(
+      date,
+      "h:mm a"
+    )} ${daysDifference} days ago.`;
   }
 }
 
@@ -38,7 +54,7 @@ export function formatDuration(duration: number) {
   const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
 
   // Format the result
-  const formattedDuration = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+  const formattedDuration = `${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
 
   return formattedDuration;
 }
--- punchcard-react-ui/src/pages/(authed)/home.tsx
+++ punchcard-react-ui/src/pages/(authed)/home.tsx
@@ -1,162 +1,191 @@
 import axios, { AxiosResponse } from "axios";
 import { useEffect, useMemo, useState } from "react";
-import { useAuthHeader, useAuthUser, useSignOut } from "react-auth-kit"
-import styles from './home.module.css';
-import Button from "components/Button"
+import { useAuthHeader, useAuthUser, useSignOut } from "react-auth-kit";
+import styles from "./home.module.css";
+import Button from "components/Button";
 import NotesBox from "components/NotesBox";
 import { apiWrapper, formatClockedInMessage } from "lib";
 
 function getStatus(authHeader: () => string) {
-  return axios.get("http://localhost:8080/status",
-    { headers: { Authorization: authHeader() } } // request config
-  ).then((response: AxiosResponse) => response.data)
+  return axios
+    .get(
+      "http://localhost:8080/status",
+      { headers: { Authorization: authHeader() } } // request config
+    )
+    .then((response: AxiosResponse) => response.data);
 }
 
 function clockIn(authHeader: () => string) {
-  return axios.post("http://localhost:8080/clock-in",
-    { time: new Date().toJSON() }, // request body
-    { headers: { Authorization: authHeader() } } // request config
-  ).then((response: AxiosResponse) => response.data)
+  return axios
+    .post(
+      "http://localhost:8080/clock-in",
+      { time: new Date().toJSON() }, // request body
+      { headers: { Authorization: authHeader() } } // request config
+    )
+    .then((response: AxiosResponse) => response.data);
 }
 
 function saveNotes(authHeader: () => string, notes: string) {
-  return axios.put("http://localhost:8080/clock-notes",
-    { notes }, // request body
-    { headers: { Authorization: authHeader() } } // request config
-  ).then((response: AxiosResponse) => response.data)
+  return axios
+    .put(
+      "http://localhost:8080/clock-notes",
+      { notes }, // request body
+      { headers: { Authorization: authHeader() } } // request config
+    )
+    .then((response: AxiosResponse) => response.data);
 }
 
 function clockOut(authHeader: () => string, notes: string) {
-  return axios.post("http://localhost:8080/clock-out",
-    {
-      // request body
-      time: new Date().toJSON(),
-      notes
-    },
-    { headers: { Authorization: authHeader() } } // request config
-  ).then((response: AxiosResponse) => response.data)
+  return axios
+    .post(
+      "http://localhost:8080/clock-out",
+      {
+        // request body
+        time: new Date().toJSON(),
+        notes,
+      },
+      { headers: { Authorization: authHeader() } } // request config
+    )
+    .then((response: AxiosResponse) => response.data);
 }
 
-
 export default function HomePage() {
-  const signOut = useSignOut()
-  const authHeader = useAuthHeader()
-  const authState = useAuthUser()
-  const [oldNotes, setOldNotes] = useState("")
-  const [notes, setNotes] = useState("")
-  const [isNotesLoading, setIsNotesLoading] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState<Error | null>(null)
+  const signOut = useSignOut();
+  const authHeader = useAuthHeader();
+  const authState = useAuthUser();
+  const [oldNotes, setOldNotes] = useState("");
+  const [notes, setNotes] = useState("");
+  const [isNotesLoading, setIsNotesLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
   const [status, setStatus] = useState({
     is_clocked_in: false,
-    clock_in_time: ""
+    clock_in_time: "",
   });
 
-  const name = useMemo(() => authState()?.first_name ?? "", [authState])
+  const name = useMemo(() => authState()?.first_name ?? "", [authState]);
 
-  useMemo(() =>
-    apiWrapper(
-      () => getStatus(authHeader)
-        .then((d: {
-          notes: string;
-          is_clocked_in: boolean;
-          clock_in_time: string;
-        }) => {
-          const { notes, ...data } = d;
-          setStatus(data)
-          setNotes(notes)
-          setOldNotes(notes)
-        }),
-      setError,
-      setIsLoading
-    ), [])
+  useMemo(
+    () =>
+      apiWrapper(
+        () =>
+          getStatus(authHeader).then(
+            (d: {
+              notes: string;
+              is_clocked_in: boolean;
+              clock_in_time: string;
+            }) => {
+              const { notes, ...data } = d;
+              setStatus(data);
+              setNotes(notes);
+              setOldNotes(notes);
+            }
+          ),
+        setError,
+        setIsLoading
+      ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
 
   useEffect(() => {
     if (axios.isAxiosError(error) && error.response?.status == 401) {
-      signOut()
+      signOut();
     }
-  }, [error])
+  }, [error, signOut]);
 
-
-  return <>
-    {error && <h3>Error: {error.message}</h3>}
-    <div className={styles.page}>
-      <div className={styles.mainArea}>
-        <h1 className={styles.title}>Welcome back {name}</h1>
-        <h2 className={styles.text}>
-          {isLoading
-            ? "Loading..."
-            : status.is_clocked_in
+  return (
+    <>
+      {error && <h3>Error: {error.message}</h3>}
+      <div className={styles.page}>
+        <div className={styles.mainArea}>
+          <h1 className={styles.title}>Welcome back {name}</h1>
+          <h2 className={styles.text}>
+            {isLoading
+              ? "Loading..."
+              : status.is_clocked_in
               ? formatClockedInMessage(new Date(status.clock_in_time))
               : "You are not currently clocked in"}
-        </h2>
+          </h2>
 
-        {!status.is_clocked_in
-          ? <Button
-            className={styles.mainButton}
-            loading={isLoading}
-            text="Clock In"
-            color="green"
-            onClick={() =>
-              apiWrapper(
-                () => clockIn(authHeader)
-                  .then((data: { is_clocked_in: boolean, clock_in_time: string }) => {
-                    setStatus(data)
-                    setNotes("")
-                    setOldNotes("")
-                  }),
-                setError,
-                setIsLoading
-              )
-            }
-          />
-          : <>
-            <NotesBox
-              disabled={isLoading || isNotesLoading}
-              text={notes}
-              onTextChange={setNotes}
-            />
-            <Button
-              className={styles.notesButton}
-              loading={isNotesLoading}
-              disabled={isLoading || notes == oldNotes}
-              text="Save Notes"
-              color="blue"
-              onClick={() =>
-                apiWrapper(
-                  () => saveNotes(authHeader, notes).then(() => setOldNotes(notes)),
-                  setError,
-                  setIsNotesLoading
-                )
-              }
-            />
+          {!status.is_clocked_in ? (
             <Button
               className={styles.mainButton}
-              loading={status.is_clocked_in && isLoading}
-              disabled={!status.is_clocked_in || isLoading || isNotesLoading}
-              text={(notes == oldNotes ? "" : "Save Notes and ") + "Clock Out"}
+              loading={isLoading}
+              text="Clock In"
               color="green"
               onClick={() =>
                 apiWrapper(
-                  () => clockOut(
-                    authHeader,
-                    notes
-                  ).then(
-                    (data: { is_clocked_in: boolean, clock_in_time: string }) => {
-                      setStatus(data)
-                      setNotes("")
-                      setOldNotes("")
-                    }
-                  ),
+                  () =>
+                    clockIn(authHeader).then(
+                      (data: {
+                        is_clocked_in: boolean;
+                        clock_in_time: string;
+                      }) => {
+                        setStatus(data);
+                        setNotes("");
+                        setOldNotes("");
+                      }
+                    ),
                   setError,
                   setIsLoading
                 )
               }
             />
-          </>
-        }
+          ) : (
+            <>
+              <NotesBox
+                disabled={isLoading || isNotesLoading}
+                text={notes}
+                onTextChange={setNotes}
+              />
+              <Button
+                className={styles.notesButton}
+                loading={isNotesLoading}
+                disabled={isLoading || notes == oldNotes}
+                text="Save Notes"
+                color="blue"
+                onClick={() =>
+                  apiWrapper(
+                    () =>
+                      saveNotes(authHeader, notes).then(() =>
+                        setOldNotes(notes)
+                      ),
+                    setError,
+                    setIsNotesLoading
+                  )
+                }
+              />
+              <Button
+                className={styles.mainButton}
+                loading={status.is_clocked_in && isLoading}
+                disabled={!status.is_clocked_in || isLoading || isNotesLoading}
+                text={
+                  (notes == oldNotes ? "" : "Save Notes and ") + "Clock Out"
+                }
+                color="green"
+                onClick={() =>
+                  apiWrapper(
+                    () =>
+                      clockOut(authHeader, notes).then(
+                        (data: {
+                          is_clocked_in: boolean;
+                          clock_in_time: string;
+                        }) => {
+                          setStatus(data);
+                          setNotes("");
+                          setOldNotes("");
+                        }
+                      ),
+                    setError,
+                    setIsLoading
+                  )
+                }
+              />
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  </>
-
+    </>
+  );
 }
