45d94f754cb19c8cf13bc6f98a8f982ec7a9f7b5
DhruvArora-03 <dhruvarora@utexas.edu>
2024-03-10 21:03:02 -0500

    linting

--- punchcard-react-ui/src/components/NavBar/NavBar.tsx
+++ punchcard-react-ui/src/components/NavBar/NavBar.tsx
@@ -1,54 +1,65 @@
-import { Link } from 'router'
-import styles from './NavBar.module.css'
-import logo from 'assets/gideon-logo.png'
-import { NavBarModes } from '.';
-import { useSignOut } from 'react-auth-kit';
-import { ReactNode } from 'react';
-import { useLocation } from 'react-router';
-import classNames from 'classnames';
-
+import { Link } from "router";
+import styles from "./NavBar.module.css";
+import logo from "assets/gideon-logo.png";
+import { NavBarModes } from ".";
+import { useSignOut } from "react-auth-kit";
+import { ReactNode } from "react";
+import { useLocation } from "react-router";
+import classNames from "classnames";
 
 type NavBarProps = {
   mode?: NavBarModes;
-}
+};
+
+// type PropsFrom<Link> = Link extends React.FC<infer Props> ? Props : never;
 
 type NavBarItemProps = {
-  to: string,
-  children: ReactNode,
-  onClick?: () => any
-}
-
+  to: string;
+  children: ReactNode;
+  onClick?: () => void;
+};
 
 export default function NavBar(props: NavBarProps) {
   const signOut = useSignOut();
   const location = useLocation();
 
   function Item(itemProps: NavBarItemProps) {
-    return <Link
-      className={classNames(styles.item, location.pathname == itemProps.to && styles.activeItem)}
-      to={itemProps.to as any}
-      onClick={itemProps.onClick}>
-      {itemProps.children}
-    </Link>
+    return (
+      <Link
+        className={classNames(
+          styles.item,
+          location.pathname == itemProps.to && styles.activeItem
+        )}
+        to={itemProps.to as any} // eslint-disable-line @typescript-eslint/no-explicit-any
+        onClick={itemProps.onClick}
+      >
+        {itemProps.children}
+      </Link>
+    );
   }
 
-  return <div className={styles.navbar}>
-    <Item to="/">
-      <img src={logo} className={styles.logo} />
-    </Item>
+  return (
+    <div className={styles.navbar}>
+      <Item to="/">
+        <img src={logo} className={styles.logo} />
+      </Item>
 
-    <div className={styles.navbarItems}>
-      {props.mode !== NavBarModes.UnAuthenticated &&
-        <>
-          <Item to="/home">Home</Item>
-          <Item to="/history">Shift History</Item>
-          <Item to="/payments">Payments</Item>
-          {props.mode == NavBarModes.Admin && <Item to="/users">Manage Instructors</Item>}
-          <Item to="/settings">Settings</Item>
-          <Item to="/login" onClick={signOut}>Log Out</Item>
-        </>
-      }
+      <div className={styles.navbarItems}>
+        {props.mode !== NavBarModes.UnAuthenticated && (
+          <>
+            <Item to="/home">Home</Item>
+            <Item to="/history">Shift History</Item>
+            <Item to="/payments">Payments</Item>
+            {props.mode == NavBarModes.Admin && (
+              <Item to="/users">Manage Users</Item>
+            )}
+            <Item to="/settings">Settings</Item>
+            <Item to="/login" onClick={signOut}>
+              Log Out
+            </Item>
+          </>
+        )}
+      </div>
     </div>
-
-  </div>
+  );
 }
